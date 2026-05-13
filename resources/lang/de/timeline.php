65a16d059ed11851ae641b1507e1d4186eb223a7
Salman Hijazi <salman.hijazi@devletes.com>
2026-05-13 15:41:48 -0400

    Use Filament's native empty state in timeline view

--- resources/lang/de/timeline.php
+++ resources/lang/de/timeline.php
@@ -8,9 +8,4 @@ return [
     'toggle_day' => 'Einträge am :day umschalten',
 
     'collapsed_summary' => '{1} 1 Beitrag ausgeblendet|[2,*] :count Beiträge ausgeblendet',
-
-    'empty_state' => [
-        'heading' => 'Noch nichts zu zeigen',
-        'description' => 'Sobald Daten verfügbar sind, werden hier Einträge angezeigt.',
-    ],
 ];
